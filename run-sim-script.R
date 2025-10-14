567fe2acf87c0975392d4e69ff646d85b89fe847
Liam Durkin <Liam.Durkin@flutteruki.com>
2025-10-14 15:14:38 +0100

    format code

--- run-sim-script.R
+++ run-sim-script.R
@@ -1,6 +1,9 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-runSim <- function(currentSeason, noOfSims, noOfChunks, ratings_input_file_path){
+runSim <- function(currentSeason,
+                   noOfSims,
+                   noOfChunks,
+                   ratings_input_file_path) {
   ratings_df <- read.csv(ratings_input_file_path)
   ratings_named_vector <- setNames(ratings_df[[2]], ratings_df[[1]])
   
@@ -13,16 +16,21 @@ runSim <- function(currentSeason, noOfSims, noOfChunks, ratings_input_file_path)
     verbosity = "NONE"
   )
   
-  arranged_output <- sims$overall |> 
+  arranged_output <- sims$overall |>
     dplyr::select(-seed1, -draft1, -draft5) |>
     dplyr::arrange(division, -div1) |>
     dplyr::mutate(
-      playoff = round(1/playoff, digits = 2),
-      div1 = round(1/div1, digits = 2),
-      won_conf = round(1/won_conf, digits = 2),
-      won_sb = round(1/won_sb, digits = 2)
+      playoff = round(1 / playoff, digits = 2),
+      div1 = round(1 / div1, digits = 2),
+      won_conf = round(1 / won_conf, digits = 2),
+      won_sb = round(1 / won_sb, digits = 2)
     ) |>
-    dplyr::rename(make_playoffs = playoff, win_div = div1, win_conf = won_conf, win_sb = won_sb)
+    dplyr::rename(
+      make_playoffs = playoff,
+      win_div = div1,
+      win_conf = won_conf,
+      win_sb = won_sb
+    )
   
   output_as_json <- jsonlite::toJSON(arranged_output)
   
@@ -32,4 +40,7 @@ runSim <- function(currentSeason, noOfSims, noOfChunks, ratings_input_file_path)
   print(arranged_output |> knitr::kable())
 }
 
-runSim(as.numeric(args[1]), as.numeric(args[2]), as.numeric(args[3]), args[4])
+runSim(as.numeric(args[1]),
+       as.numeric(args[2]),
+       as.numeric(args[3]),
+       args[4])
--- updateRatings.R
+++ updateRatings.R
@@ -1,15 +1,13 @@
-updateRatings <- function(){
-  nfeloRatings <- read.csv("https://raw.githubusercontent.com/greerreNFL/nfelo/refs/heads/main/output_data/elo_snapshot.csv")
+updateRatings <- function() {
+  nfeloRatings <- read.csv(
+    "https://raw.githubusercontent.com/greerreNFL/nfelo/refs/heads/main/output_data/elo_snapshot.csv"
+  )
   
   ratings <- nfeloRatings |>
     dplyr::select(team, rating = nfelo_base) |>
     dplyr::mutate(
       rating = round(rating),
-      team = dplyr::case_when(
-        team == "LAR" ~ "LA",
-        team == "OAK" ~ "LV",
-        TRUE ~ team
-      )
+      team = dplyr::case_when(team == "LAR" ~ "LA", team == "OAK" ~ "LV", TRUE ~ team)
     )
   
   write.csv(ratings, "./team_ratings.csv", row.names = FALSE)
