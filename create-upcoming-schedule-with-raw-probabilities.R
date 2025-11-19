8ba8460f896232286b1178d0b2ec1bb886c6f790
Liam Durkin <Liam.Durkin@flutteruki.com>
2025-11-19 08:59:27 +0000

    moved to new simulation logic permanently.

--- create-upcoming-schedule-with-raw-probabilities.R
+++ create-upcoming-schedule-with-raw-probabilities.R
@@ -1,4 +1,4 @@
-createUpcomingScheduleWithProbabilities <- function(){
+createUpcomingScheduleWithProbabilities <- function() {
   schedule <- nflreadr::load_schedules(nflreadr::get_current_season()) |>
     dplyr::filter(game_type == "REG", week >= nflreadr::get_current_week()) |>
     dplyr::mutate(
@@ -8,14 +8,24 @@ createUpcomingScheduleWithProbabilities <- function(){
     dplyr::rowwise() |>
     dplyr::mutate(
       implied = list(implied::implied_probabilities(
-        c(home_decimal_odds, away_decimal_odds),
-        method = "power"
+        c(home_decimal_odds, away_decimal_odds), method = "power"
       )),
       home_prob = implied$probabilities[1],
       away_prob = implied$probabilities[2]
     ) |>
     dplyr::ungroup() |>
-    dplyr::select(game_type, week, away_team, home_team, away_rest, home_rest, location, result, away_prob, home_prob)
+    dplyr::select(
+      game_type,
+      week,
+      away_team,
+      home_team,
+      away_rest,
+      home_rest,
+      location,
+      result,
+      away_prob,
+      home_prob
+    )
   
   return(schedule)
 }
